cc66530a514c856d0c0ecea70e60f3d824836ca6
risha <rishadjhangiani@gmail.com>
2022-06-07 07:26:33 -0700

    finished glasshouse

---
+++ glasshouse/script.js
@@ -0,0 +1,14 @@
+function show_hide() {
+    var click = document.getElementById("list-items");
+    if(click.style.display ==="none") {
+       click.style.display ="block";
+    } else {
+       click.style.display ="none";
+    } 
+ };
+
+window.open(
+    'https://dancestudio-pro.com/online/glasshousedance',
+    '_blank'
+);
+
